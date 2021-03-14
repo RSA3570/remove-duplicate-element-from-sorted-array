7e8c78961a26ab366ade948404144f11e8d9496a
RSA3570 <roushankb17cs@student.aravalicollege.org>
2021-03-14 19:59:41 +0530

    changes

--- RemoveDuplicacyUsingHashSet.java
+++ RemoveDuplicacyUsingHashSet.java
@@ -16,3 +16,19 @@ public class RemoveDuplicacyUsingHashSet {
          }
       }
 }
+public class RemoveDuplicacyUsingHashSet {
+  public static void main(String[] args) {
+    
+    int arr[] = {1,2,2,3,4,5,5};
+    HashSet<Integer> hs = new HashSet<>();
+    
+    for(int i = 0; i<arr.length-1; i++){
+      hs.add(arr[i]);
+    }
+    
+     
+     for(int num: hs){
+       System.out.print(num+", ");
+     }
+  }
+}
