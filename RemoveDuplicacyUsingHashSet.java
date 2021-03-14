224c5531f004351f4c1bd7d73d7dde0c89498aba
RSA3570 <roushankb17cs@student.aravalicollege.org>
2021-03-14 20:06:26 +0530

    Remove duplicacy using hashset in sorted and unsorted array

--- RemoveDuplicacyUsingHashSet.java
+++ RemoveDuplicacyUsingHashSet.java
@@ -6,29 +6,13 @@ public class RemoveDuplicacyUsingHashSet {
         int arr[] = {1,2,2,3,4,5,5};
         HashSet<Integer> hs = new HashSet<>();
         
-        for(int i = 0; i<arr.length-1; i++){
+        for(int i = 0; i<arr.length; i++){
           hs.add(arr[i]);
         }
         
          
-         for(int num: hs){
-           System.out.print(hs+", ");
+         for(int num : hs){
+           System.out.print(num+" ");
          }
       }
-}
-public class RemoveDuplicacyUsingHashSet {
-  public static void main(String[] args) {
-    
-    int arr[] = {1,2,2,3,4,5,5};
-    HashSet<Integer> hs = new HashSet<>();
-    
-    for(int i = 0; i<arr.length-1; i++){
-      hs.add(arr[i]);
-    }
-    
-     
-     for(int num: hs){
-       System.out.print(num+", ");
-     }
-  }
 }
